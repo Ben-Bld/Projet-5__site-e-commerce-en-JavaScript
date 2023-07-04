728a9bc957370084e12e0378bc7d8d01a5f5c5ce
Ben-Bld <121769337+Ben-Bld@users.noreply.github.com>
2023-07-04 16:09:03 +0200

    E

--- front/js/script.js
+++ front/js/script.js
@@ -1,25 +1,28 @@
-// <!-- <a href="./product.html?id=42">
-// <article>
-//   <img src=".../product01.jpg" alt="Lorem ipsum dolor sit amet, Kanap name1">
-//   <h3 class="productName">Kanap name1</h3>
-//   <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
-// </article> -->
 
+//initialisation
+//variables de base
 const localhost = "http://localhost:3000/api/products";
 
 const box = document.getElementById("items");
 
-let card = document.createElement("div");
-
-box.append(card);
 
 
 
-box.innerHTML += `<a href="./product.html?id=42">
- <article>
-   <img src=".../product01.jpg" alt="Lorem ipsum dolor sit amet, Kanap name1">
-  <h3 class="productName">Kanap name1</h3>
-  <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
- </article>`;
+fetch('http://localhost:3000/api/products')
+.then(response => response.json())
+.then(data => {console.log(data)
+  for(i = 0; i < data.length; i++){
+  
+    box.innerHTML += `<a href="./product.html?id=42">
+     <article>
+       <img src="${data[i].imageUrl}" alt="Lorem ipsum dolor sit amet, Kanap name1">
+      <h3 class="productName">${data[i].name} </h3>
+      <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
+     </article>`;
+    
+    }});
+
+
+
 
  
